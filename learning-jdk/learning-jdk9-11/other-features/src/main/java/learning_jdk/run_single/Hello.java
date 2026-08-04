ff9347061e9df3b70bd7447332d81978ff0f1c92
my <my@123hhh.com>
2026-08-04 11:12:59 +0800

    Java9-25新特性 13Java9-11新特性：单文件源代码程序 https://www.bilibili.com/video/BV1jPkzBGEJy

---
+++ learning-jdk/learning-jdk9-11/other-features/src/main/java/learning_jdk/run_single/Hello.java
@@ -0,0 +1,16 @@
+package learning_jdk.run_single;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    // before java 11
+    // cd learning-jdk9-11\other-features\src\main\java
+    // javac learning_jdk\run_single\Hello.java
+    // java learning_jdk.run_single.Hello
+
+    // after java 11
+    // java learning_jdk/run_single/Hello.java
+}
